ac71ef1e81cc08e54fcb1f337f788d126b4ffca8
Ricardo Silveira de Castro Pires <ricardo0605@hotmail.com.br>
2019-08-18 20:33:41 +0100

    Adding AJAX call to search zip code info.

--- src/App/wwwroot/js/site.js
+++ src/App/wwwroot/js/site.js
@@ -5,12 +5,12 @@
 
             $("a[data-modal]").on("click",
                 function (e) {
-                    $('#myModalContent').load(this.href,
+                    $("#myModalContent").load(this.href,
                         function () {
-                            $('#myModal').modal({
+                            $("#myModal").modal({
                                 keyboard: true
                             },
-                                'show');
+                                "show");
                             bindForm(this);
                         });
                     return false;
@@ -18,17 +18,17 @@
         });
 
         function bindForm(dialog) {
-            $('form', dialog).submit(function () {
+            $("form", dialog).submit(function () {
                 $.ajax({
                     url: this.action,
                     type: this.method,
                     data: $(this).serialize(),
                     success: function (result) {
                         if (result.success) {
-                            $('#myModal').modal('hide');
-                            $('#AddressTarget').load(result.url); // Load Html result to div
+                            $("#myModal").modal("hide");
+                            $("#AddressTarget").load(result.url); // Load Html result to div
                         } else {
-                            $('#myModalContent').html(result);
+                            $("#myModalContent").html(result);
                             bindForm(dialog);
                         }
                     }
@@ -36,5 +36,54 @@
                 return false;
             });
         }
+    });
+}
+
+function SearchZipCode() {
+    $(document).ready(function () {
+
+        function clear_zip_data() {
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        $("#Address_ZipCode").blur(function () {
+            var zipCode = $(this).val().replace(/\D/g, '');
+
+            if (zipCode !== "") {
+
+                var validZipCode = /^[0-9]{8}$/;
+
+                if (validZipCode.test(zipCode)) {
+                    $("#Address_Street").val("loading...");
+                    $("#Address_Neighborhood").val("loading...");
+                    $("#Address_City").val("loading...");
+                    $("#Address_State").val("loading...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + zipCode + "/json/?callback=?",
+                        function (dados) {
+
+                            if (!("erro" in dados)) {
+                                $("#Address_Street").val(dados.logradouro);
+                                $("#Address_Neighborhood").val(dados.bairro);
+                                $("#Address_City").val(dados.localidade);
+                                $("#Address_State").val(dados.uf);
+                            }
+                            else {
+                                clear_zip_data();
+                                alert("Zip Code not fount.");
+                            }
+                        });
+
+                } else {
+                    clear_zip_data();
+                    alert("Invalid Zip Code format.");
+                }
+            } else {
+                clear_zip_data();
+            }
+        });
     });
 }
